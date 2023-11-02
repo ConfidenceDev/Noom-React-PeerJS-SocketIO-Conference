deed3c930873fc2f4545c824b7ee92338261eaa4
Dev-Geek <confidostic3@gmail.com>
2023-11-02 22:56:25 +0100

    Fix 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Login, Room } from "./components"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
@@ -9,13 +9,13 @@ export default function App() {
   return (
     <>
       <Provider store={store}>
-        <ToastContainer className="toast" />
-        <Router>
+        <BrowserRouter>
+          <ToastContainer className="toast" />
           <Routes>
             <Route path="/lecture/:room" element={<Login />} />
             <Route path="/lecture/:room/:access" element={<Room />} />
           </Routes>
-        </Router>
+        </BrowserRouter>
       </Provider>
     </>
   )
