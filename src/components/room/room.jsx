9d7b0ba86d6a9f0f2054c078e884087f9f9c2894
Dev-Geek <confidostic3@gmail.com>
2023-11-21 10:52:10 +0100

    Fix 12

--- src/components/room/room.jsx
+++ src/components/room/room.jsx
@@ -338,7 +338,7 @@ export default function Room() {
     videoElement.srcObject = stream
     videoElement.id = userID
     videoElement.className = userID
-    //videoElement.muted = true
+    videoElement.muted = false
     videoElement.classList.add("video-stream")
     videoElement.setAttribute("autoplay", "")
     videoElement.setAttribute("playsinline", "")
